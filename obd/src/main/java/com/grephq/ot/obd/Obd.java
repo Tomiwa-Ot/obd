059e300765489b0c9af7189b82975193879af9cb
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2023-06-20 22:55:52 +0100

    added obd helper methods

--- obd/src/main/java/com/grephq/ot/obd/Obd.java
+++ obd/src/main/java/com/grephq/ot/obd/Obd.java
@@ -14,6 +14,11 @@ import android.hardware.usb.UsbManager;
 import androidx.core.app.ActivityCompat;
 
 import com.grephq.ot.obd.Command.AT;
+import com.grephq.ot.obd.Command.Mode01;
+import com.grephq.ot.obd.Command.Mode03;
+import com.grephq.ot.obd.Command.Mode04;
+import com.grephq.ot.obd.Command.Mode07;
+import com.grephq.ot.obd.Command.Mode09;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -197,4 +202,415 @@ public class Obd {
 
     }
 
+    /**
+     * Get engine coolant temperature
+     *
+     * @return engine coolant temperature °C
+     */
+    public double getEngineCoolantTemperature() throws IOException {
+        String response = sendCommand(Mode01.ENGINE_COOLANT_TEMPERATURE);
+        return 0;
+    }
+
+    /**
+     * Get minimum engine coolant temperature
+     *
+     * @return minimum engine coolant temperature °C
+     */
+    public double getMinimumVehicleCoolantTemperature() {
+        return -40;
+    }
+
+    /**
+     * Get maximum engine coolant temperature
+     *
+     * @return maximum engine coolant temperature °C
+     */
+    public double getMaximumVehicleCoolantTemperature() {
+        return 215;
+    }
+
+    /**
+     * Get calculated engine load
+     *
+     * @return calculated engine load (%)
+     */
+    public double getEngineLoad() throws IOException {
+        String response = sendCommand(Mode01.ENGINE_LOAD);
+        return 0;
+    }
+
+    /**
+     * Get minimum engine load
+     *
+     * @return minimum engine load (%)
+     */
+    public double getMinimumEngineLoad() {
+        return 0;
+    }
+
+    /**
+     * Get maximum engine load
+     *
+     * @return maximum engine load (%)
+     */
+    public double getMaximumEngineLoad() {
+        return 100;
+    }
+
+    /**
+     * Get engine oil temperature
+     *
+     * @return engine oil temperature in °C
+     */
+    public double getEngineOilTemperature() throws IOException {
+        String response = sendCommand(Mode01.ENGINE_OIL_TEMPERATURE);
+        return 0;
+    }
+
+    /**
+     * Get minimum oil temperature
+     *
+     * @return minimum oil temperature in °C
+     */
+    public double getMinimumEngineOilTemperature() {
+        return -40;
+    }
+
+    /**
+     * Get maximum oil temperature
+     *
+     * @return maximum oil temperature in °C
+     */
+    public double getMaximumEngineOilTemperature() {
+        return 210;
+    }
+
+    /**
+     * Get relative accelerator pedal position
+     *
+     * @return relative accelerator pedal position (%)
+     */
+    public double getRelativeAcceleratorPedalPosition() throws IOException {
+        String response = sendCommand(Mode01.RELATIVE_ACCELERATOR_PEDAL_POSITION);
+        return 0;
+    }
+
+    /**
+     * Get minimum relative accelerator pedal position
+     *
+     * @return minimum relative accelerator pedal position (%)
+     */
+    public double getMinimumRelativeAcceleratorPedalPosition() {
+        return 0;
+    }
+
+    /**
+     * Get maximum relative accelerator pedal position
+     *
+     * @return maximum relative accelerator pedal position (%)
+     */
+    public double getMaximumRelativeAcceleratorPedalPosition() {
+        return 100;
+    }
+
+    /**
+     * Get hybrid battery pack remaining life
+     *
+     * @return hybrid battery pack remaining life (%)
+     */
+    public double getHybridBatteryPackRemainingLife() throws IOException {
+        String response = sendCommand(Mode01.HYBRID_BATTERY_PACK_REMAINING_LIFE);
+        return 0;
+    }
+
+    /**
+     * Get minimum hybrid battery pack remaining life
+     *
+     * @return minimum hybrid battery pack remaining life (%)
+     */
+    public double getMinimumHybridBatteryPackRemainingLife() {
+        return 0;
+    }
+
+    /**
+     * Get maximum hybrid battery pack remaining life
+     *
+     * @return maximum hybrid battery pack remaining life (%)
+     */
+    public double getMaximumHybridBatteryPackRemainingLife() {
+        return 100;
+    }
+
+    /**
+     * Get fuel injection timing
+     *
+     * @return fuel injection timing (°)
+     */
+    public double getFuelInjectionTiming() throws IOException {
+        String response = sendCommand(Mode01.FUEL_INJECTION_TIMING);
+        return 0;
+    }
+
+    /**
+     * Get minimum fuel injection timing
+     *
+     * @return minimum fuel injection timing (°)
+     */
+    public double getMinimumFuelInjectionTiming() {
+        return -210.00;
+    }
+
+    /**
+     * Get maximum fuel injection timing
+     *
+     * @return maximum fuel injection timing (°)
+     */
+    public double getMaximumFuelInjectionTiming() {
+        return 301.992;
+    }
+
+    /**
+     * Get engine fuel rate
+     *
+     * @return engine fuel rate (L/h)
+     */
+    public double getEngineFuelRate() throws IOException {
+        String response = sendCommand(Mode01.ENGINE_FUEL_RATE);
+        return 0;
+    }
+
+    /**
+     * Get minimum engine fuel rate
+     *
+     * @return minimum engine fuel rate (L/h)
+     */
+    public double getMinimumEngineFuelRate() {
+        return 0;
+    }
+
+    /**
+     * Get maximum engine fuel rate
+     *
+     * @return maximum engine fuel rate (L/h)
+     */
+    public double getMaximumEngineFuelRate() {
+        return 3212.75;
+    }
+
+    /**
+     * Get fuel pressure
+     *
+     * @return fuel pressure in kPa
+     */
+    public double getFuelPressure() throws IOException {
+        String response = sendCommand(Mode01.FUEL_PRESSURE);
+        return 0;
+    }
+
+    /**
+     * Get minimum fuel pressure
+     *
+     * @return minimum fuel pressure in kPa
+     */
+    public double getMinimumFuelPressure() {
+        return 0;
+    }
+
+    /**
+     * Get maximum fuel pressure
+     *
+     * @return maximum fuel pressure in kPa
+     */
+    public double getMaximumFuelPressure() {
+        return 765;
+    }
+
+    /**
+     * Get vehicle intake air temperature
+     *
+     * @return vehicle intake air temperature (°C)
+     */
+    public double getVehicleIntakeAirTemperature() throws IOException {
+        String response = sendCommand(Mode01.INTAKE_AIR_TEMPERATURE);
+        return 0;
+    }
+
+    /**
+     * Get minimum vehicle intake air temperature
+     *
+     * @return minimum vehicle intake air temperature (°C)
+     */
+    public double getMinimumVehicleIntakeAirTemperature() {
+        return -40;
+    }
+
+    /**
+     * Get maximum vehicle intake air temperature
+     *
+     * @return maximum vehicle intake air temperature (°C)
+     */
+    public double getMaximumVehicleIntakeAirTemperature() {
+        return 215;
+    }
+
+    /**
+     * Get throttle position
+     *
+     * @return throttle position (%)
+     */
+    public double getThrottlePosition() throws IOException {
+        String response = sendCommand(Mode01.THROTTLE_POSITION);
+        return 0;
+    }
+
+    /**
+     * Get minimum throttle position
+     *
+     * @return minimum throttle position (%)
+     */
+    public double getMinimumThrottlePosition() {
+        return 0;
+    }
+
+    /**
+     * Get maximum throttle position
+     *
+     * @return maximum throttle position (%)
+     */
+    public double getMaximumThrottlePosition() {
+        return 100;
+    }
+
+    /**
+     * Get vehicle speed
+     *
+     * @return vehicle speed in km/h
+     */
+    public double getVehicleSpeed() throws IOException {
+        String response = sendCommand(Mode01.VEHICLE_SPEED);
+        return 0;
+    }
+
+    /**
+     * Get minimum vehicle speed
+     *
+     * @return minimum vehicle speed in km/h
+     */
+    public double getMinimumVehicleSpeed() {
+        return 0;
+    }
+
+    /**
+     * Get maximum vehicle speed
+     *
+     * @return maximum vehicle speed in km/h
+     */
+    public double getMaximumVehicleSpeed() {
+        return 255;
+    }
+
+    /**
+     * Get engine speed
+     *
+     * @return engine speed (rpm)
+     */
+    public double getEngineSpeed() throws IOException {
+        String response = sendCommand(Mode01.ENGINE_SPEED);
+        return 0;
+    }
+
+    /**
+     * Get exhaust pressure
+     *
+     * @return exhaust pressure
+     */
+    public double getExhaustPressure() throws IOException {
+        String response = sendCommand( Mode01.EXHAUST_PRESSURE);
+        return 0;
+    }
+
+    /**
+     * Get turbocharger rpm
+     *
+      * @return turbocharger rpm
+     */
+    public double getTurbochargerRPM() throws IOException {
+        String response = sendCommand(Mode01.TURBOCHARGER_RPM);
+        return 0;
+    }
+
+    /**
+     * Get minimum engine speed
+     *
+     * @return minimum engine speed (rpm)
+     */
+    public double getMinimumEngineSpeed() {
+        return 0;
+    }
+
+    /**
+     * Get maximum engine speed
+     *
+     * @return maximum engine speed (rpm)
+     */
+    public double getMaximumEngineSpeed() {
+        return 16383.75 ;
+    }
+
+    /**
+     * Show stored Diagnostic Trouble Codes (DTCs)
+     * @return Diagnostic Trouble Codes
+     */
+    public String requestTroubleCodes() throws IOException {
+        String response = sendCommand(Mode03.GET_DIAGNOSTIC_TROUBLE_CODES);
+        return "";
+    }
+
+    /**
+     * Clear trouble codes / Malfunction indicator lamp (MIL) / Check engine light
+     */
+    public void clearDiagnosticTroubleCodes() throws IOException {
+        sendCommand(Mode04.CLEAR_DIAGNOSTIC_TROUBLE_CODES);
+    }
+
+    /**
+     * Get pending trouble codes
+     *
+     * @return pending trouble codes
+     */
+    public String getPendingTroubleCodes() throws IOException {
+        String response = sendCommand(Mode07.GET_CURRENT_DTC);
+        return "";
+    }
+
+    /**
+     * Get calibration ID
+     *
+     * @return Get calibration ID
+     */
+    public String getCalibrationID() throws IOException {
+        String response = sendCommand(Mode09.CALIBRATION_ID);
+        return "";
+    }
+
+    /**
+     * Get vehicle identification number
+     *
+     * @return vehicle identification number
+     */
+    public String getVehicleIdentificationNumber() throws IOException {
+        String response = sendCommand(Mode09.VEHICLE_IDENTIFICATION_NUMBER);
+        return "";
+    }
+
+    /**
+     * Get ECU name
+     *
+     * @return ECU name
+     */
+    public String getECUName() throws IOException {
+        String response = sendCommand(Mode09.ECU_NAME);
+        return "";
+    }
 }
